cb5f007491670fb9490bdafdff43249dbd46ec36
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-05-08 08:59:33 -0400

    add pool ball objects

---
+++ src/components/objects/Ball/Ball.js
@@ -0,0 +1,25 @@
+import { Group, LoadingManager } from 'three';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import MATERIAL from './8Ball.mtl';
+import OBJ from './8Ball.obj';
+
+class Ball extends Group {
+    constructor() {
+        super();
+
+        this.name = 'table';
+
+        const manager = new LoadingManager();
+
+        new MTLLoader(manager).load(MATERIAL, (materials) => {
+            materials.preload();
+
+            new OBJLoader(manager)
+                .setMaterials(materials)
+                .load(OBJ, (object) => this.add(object));
+        });
+    }
+}
+
+export default Ball;
--- src/components/scenes/TableScene.js
+++ src/components/scenes/TableScene.js
@@ -1,5 +1,5 @@
-import { Scene, Color } from 'three';
-import { Table } from 'objects';
+import { Scene, Color, Vector3, Box3, EventDispatcher } from 'three';
+import { Table, Ball } from 'objects';
 import { BasicLights } from 'lights';
 
 class TableScene extends Scene {
@@ -19,6 +19,13 @@ class TableScene extends Scene {
         const table = new Table();
         const lights = new BasicLights();
         this.add(table, lights);
+        for (let i = 0; i < 3; i++) {
+            let ball = new Ball();
+            this.add(ball);
+            ball.position.y = 0.72;
+            ball.position.z = 0 - 0.2;
+            ball.position.x = 0.2 * i + 0.2;
+        }
     }
 
     addToUpdateList(object) {
